cbd41fac019bfa219682ca7f36a8fbbf536cedd3
D-Pats <devinpatel97@gmail.com>
2022-08-18 22:33:52 +0100

    1688_Count_of_Matches_in_Tournament

---
+++ 1688_Count_of_Matches_in_Tournament/1688_Count_of_Matches_in_Tournament.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfMatches(int n) {
+		int matches = 0;
+
+		while (n > 1) {
+			if (n % 2 == 0) {
+				n /= 2;
+				matches += n;
+			} else {
+				n++;
+				n /= 2;
+				matches += n - 1;
+			}
+		}
+        return matches;
+    }
+
+}
